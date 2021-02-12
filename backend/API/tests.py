2033a6d17d4074c8f88450d5b911803d9c74bbc2
Solveig Kruger <solvehk@stud.ntnu.no>
2021-02-12 15:37:28 +0100

    test profil, funker ikke helt #9

--- backend/API/tests.py
+++ backend/API/tests.py
@@ -1,26 +1,43 @@
 # from django.test import TestCase
-
+# from .models import Profile
 # Create your tests here.
-""" from datetime import datetime
-from django.test import TestCase
+# """ from datetime import datetime
+# from django.test import TestCase
 
-from .models import Profile, Ad
+# from .models import Profile, Ad
 
-# (1)
-class ProfileModelTest(TestCase):
-    # (2)
-    @classmethod
-    def setUpTestData(self):
-        self.user1 = Profile.objects.create()
-        first_name="John"
-        last_name="Doe"
+# # (1)
+# class ProfileModelTest(TestCase):
+#     # (2)
+#     @classmethod
+#     def setUpTestData(self):
+#         self.user1 = Profile.objects.create()
+#         first_name="John"
+#         last_name="Doe"
 
 
-    # (3)
-    def test_it_has_information_fields(self):
-        self.assertIsInstance(self.user1.first_name, str)
-        self.assertIsInstance(self.user1.last_name, str)
- """
+#     # (3)
+#     def test_it_has_information_fields(self):
+#         self.assertIsInstance(self.user1.first_name, str)
+#         self.assertIsInstance(self.user1.last_name, str)
+#  """
 # (4)
 # def test_it_has_timestamps(self):
 # self.assertIsInstance(self.user1.last_update, datetime)
+
+
+# class ProfileModelTest(TestCase):
+#     # (2)
+#     @classmethod
+#     def setUpTestData(cls):
+#         user1 = Profile.objects.create()
+
+#         ##user1.city = "Dee"
+#         # print(user1)
+
+#     # (3)
+#     def test_it_has_information_fields(self):
+#         user1.city = "Trondheim"
+#         assertIsInstance(user1.city, str)
+#         # self.assertIsInstance(self.user1.city, str)
+#         # self.assertIsInstance(self.user1.last_name, str)
