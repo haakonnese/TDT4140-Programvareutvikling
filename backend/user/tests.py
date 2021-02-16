e246dff2976eb6ac8163d44879acca55d71196d0
Bendik Holter <bendikholter@Bendiks-MacBook-Pro.local>
2021-02-16 17:17:52 +0100

    La til flere tester #2

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -1,74 +1,83 @@
-# import unittest
-# from django.test import Client
-# from django.contrib.auth.models import User
-# from django.db import Error
-# import io
-# from rest_framework.parsers import JSONParser
+import unittest
+from django.contrib.auth.models import User
+from django.db import Error
+from rest_framework.test import APIClient
 
-# # Testene lager en ny person i databsen,
-# # slik at id-en inkrementerer hver gang man kjører testene.
-# # Kanskje mulig å registrere bruker uten at dette skjer?
+# Testene lager en ny person i databsen,
+# slik at id-en inkrementerer hver gang man kjører testene.
+# Kanskje mulig å registrere bruker uten at dette skjer?
 
 
-# class ProfileTest(unittest.TestCase):
-#     def setUp(self):
-#         # Hver test trenger en klient
-#         self.client = Client()
+class ProfileTest(unittest.TestCase):
+    def setUp(self):
+        # Hver test trenger en klient
+        self.client = APIClient()
 
-#     def test_register(self):
-#         # Lager en post forespørsel
-#         d = {
-#             "username": "john",
-#             "password": "smith",
-#             "firstname": "John",
-#             "lastname": "Doe",
-#             "phone": "33333333",
-#             "city": "Trondheim",
-#             "birth_year": "2000",
-#             "email": "JD@gmail.com",
-#         }
-#         response = self.client.post("/api/user/register", d)
+    def test_register(self):
+        # Lager en post forespørsel
+        d = {
+            "user": {
+                "email": "t@gmail.com",
+                "password": "smith",
+                "first_name": "Test",
+                "last_name": "Tester",
+                "username": "testtestD",
+            },
+            "birth_year": 2000,
+            "city": "Trondheim",
+            "phone": 22222222,
+        }
+        response = self.client.post("/api/user/register", d, format="json")
 
-#         # Henter brukeren
-#         user = User.objects.get(username="john")
+        # Sjekk at responsen er 201 user_created.
+        self.assertEqual(response.status_code, 201)
 
-#         # Sjekk at responsen er 200 OK.
-#         self.assertEqual(response.status_code, 200)
+        # Henter brukeren
+        user = User.objects.get(username="testtestD")
 
-#         # Sjekk at attributtene er satt riktig
-#         self.assertEqual(user.first_name, d["firstname"])
-#         self.assertEqual(user.last_name, d["lastname"])
-#         self.assertEqual(user.email, d["email"])
-#         self.assertEqual(user.profile.phone, d["phone"])
-#         self.assertEqual(user.profile.city, d["city"])
-#         self.assertEqual(user.profile.birth_year, d["birth_year"])
-#         self.assertEqual(user.username, d["username"])
+        # Sjekk at attributtene er satt riktig
+        self.assertEqual(user.first_name, d["user"]["first_name"])
+        self.assertEqual(user.last_name, d["user"]["last_name"])
+        self.assertEqual(user.email, d["user"]["email"])
+        self.assertEqual(user.profile.phone, str(d["phone"]))
+        self.assertEqual(user.profile.city, d["city"])
+        self.assertEqual(user.profile.birth_year, str(d["birth_year"]))
+        self.assertEqual(user.username, d["user"]["username"])
 
-#         # Sletter brukeren
-#         user.delete()
+        # Sletter brukeren
+        user.delete()
 
-#         # Lager get request som ikke er støttet
-#         respons = self.client.get("/api/user/register")
+        # Lager get request som ikke er støttet
+        respons = self.client.get("/api/user/register")
 
-#         # sjekk at responsen er 405 method not allowed
-#         self.assertEqual(respons.status_code, 405)
+        # sjekk at responsen er 405 method not allowed
+        self.assertEqual(respons.status_code, 405)
 
-#     def test_login_logout(self):
-#         d = {"username": "johnD", "password": "smith"}
-#         response = self.client.post("/api/user/login", d)
+    def test_login_logout(self):
+        d = {"username": "bruker_i_testene", "password": "smith1234"}
+        response = self.client.post("/api/user/login", d)
+        token_dict = response.data
 
-#         # Henter brukeren
-#         user = User.objects.get(username="johnD")
+        # Legger til autoriseringstoken
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token_dict["token"])
 
-#         # Sjekk at responsen er 200 OK.
-#         self.assertEqual(response.status_code, 200)
+        # Henter brukeren
+        user = User.objects.get(username="bruker_i_testene")
 
-#         # sjekker at brukeren er innlogget.
-#         d = self.client.get("/api/user/user").content
-#         stream = io.BytesIO(d)
-#         d = JSONParser().parse(stream)
-#         self.assertEqual(user.email, d["user"]["email"])
-#         self.assertEqual(user.profile.phone, d["phone"])
-#         self.assertEqual(user.profile.city, d["city"])
-#         self.assertEqual(user.profile.birth_year, d["birth_year"])
-#         self.assertEqual(user.username, d["user"]["username"])
+        # Sjekker at brukeren er innlogget.
+        d = self.client.get("/api/user/user").data
+        self.assertEqual(user.email, d["user"]["email"])
+        self.assertEqual(user.profile.phone, d["phone"])
+        self.assertEqual(user.profile.city, d["city"])
+        self.assertEqual(user.profile.birth_year, d["birth_year"])
+        self.assertEqual(user.username, d["user"]["username"])
+
+        # Fjerner token
+        self.client.credentials()
+
+        # Sjekker at brukern er utlogget
+        response = self.client.get("/api/user/user")
+        if response.status_code == 403:
+            pass
+        else:
+            raise Error()
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,18 +1,11 @@
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from .serializers import ProfileSerializer
-from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from rest_framework import status
-
-# Postman mangler CSRF-token for å kunne sende POST-forespørsel.
-# For at det skal fungere ble csrf_exempt lagt til,
-# som fjerner beskyttelsen, dette bør kanskje endres senere.
-# Vet ikke hvordan dette fikses enda.
 
 
 @api_view(["POST"])
@@ -23,22 +16,20 @@ def register_profile(request):
         serializer = ProfileSerializer(data=request.data)
         # checks if data is valid
         if serializer.is_valid(raise_exception=ValueError):
-            username = serializer.create(validated_data=request.data)
-            user = User.objects.get(username=username)
-            token = Token.objects.get(user=user)
+            user = serializer.create(validated_data=request.data)
+            token = Token.objects.get(user=user.user)
             return Response({"token": token.key}, status=status.HTTP_201_CREATED)
-
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["GET"])
 def get_profile(request):
-    """Returnerer profilen til den påloggede brukeren i JSON format."""
+    """Returns the profile of the user currently logged on."""
     response = Response()
     if request.user.is_anonymous:
         return Response(status=status.HTTP_403_FORBIDDEN)
-    json = JSONRenderer().render(ProfileSerializer(request.user.profile).data)
-    response.write(json)
-    response.status_code(status.HTTP_200_OK)
+    response.data = ProfileSerializer(request.user.profile).data
+    response.status_code = status.HTTP_200_OK
     return response
 
 
