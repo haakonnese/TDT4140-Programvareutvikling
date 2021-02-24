acbc2ea064d028df30f675480f4f2e0c326d2059
Håkon Nese <haakon.nese@gmail.com>
2021-02-24 12:17:57 +0100

    Endre avstand topp annonse #21

--- frontend/src/marketplace/components/ProductInfo/styles.js
+++ frontend/src/marketplace/components/ProductInfo/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   main: {
     margin: "auto",
-    paddingTop: "100px",
+    paddingTop: "30px",
     maxWidth: "60%",
     justify: "center",
   },
