1c97e6578b212291446de5f2acecb5f414b3047e
Jostein Tandberg <jostetan@stud.ntnu.no>
2021-03-22 08:28:42 +0100

    EditUser test ferdig

--- frontend/src/user/UserInfo/EditUser.test.js
+++ frontend/src/user/UserInfo/EditUser.test.js
@@ -2,7 +2,9 @@ import { screen } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import "@testing-library/jest-dom/extend-expect";
-import { PostData, GetData } from "../../service/FetchData";
+import { GetData} from "../../service/FetchData";
+import { PostPutData} from "../../service/FetchData";
+
 import EditUser from "../UserInfo/EditUser";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
@@ -13,27 +15,69 @@ import {
   toYoungError,
   toOldError,
 } from "../errorMessages";
-jest.mock("../../service/FetchData", () => ({
-    PostData: jest.fn(),
-  }));
+
 jest.mock("../../service/FetchData", () => ({
   GetData: jest.fn(),
+  PostPutData: jest.fn(),
 }));
+
+
+
 
 
 let container,
   firstName,
   lastName,
-  username,
   tel,
   city,
   birthYear,
-  password,
-  password2,
+  
   button;
+
+const profile = 
+  {
+    user: { first_name: "Ole", last_name: "Oleson" },
+    birth_year: "1998",
+    phone: "99576480",
+    city: "Oslo",
+  
+  
+  }
+    
+  ;
+
+
+// describe("Products component", () => {
+//   // use async and await in act when expecting component
+//   // to render with a promise inside
+//   test("if userinfo firstname is correct", async () => {
+//     const container = document.createElement("div");
+//     document.body.appendChild(container);
+    
+//     GetData.mockImplementation(() => Promise.resolve(profile));
+//     await act(async () => {
+//       ReactDOM.render(
+//         <Router>
+//           <EditUser />
+//         </Router>,
+//         container
+//       );
+//     });
+//     const profiletxt = container.querySelector(
+//      "#firstName"
+//      );
+     
+//      console.log(profiletxt.value)
+//     expect(profiletxt).toBeInTheDocument();
+    
+
+   
+//   });
+// });
 
 beforeEach(async () => {
   container = document.createElement("div");
+  GetData.mockImplementation(() => Promise.resolve(profile));
   act(() => {
     ReactDOM.render(
       <Router>
@@ -42,6 +86,7 @@ beforeEach(async () => {
       container
     );
   });
+
   document.body.appendChild(container);
 
   firstName = container.querySelector("#firstName");
@@ -50,9 +95,7 @@ beforeEach(async () => {
   lastName = container.querySelector("#lastName");
   userEvent.type(lastName, "Normann");
 
-  username = container.querySelector("#username");
-  userEvent.type(username, "ola@normann.no");
-
+  
   tel = container.querySelector("#tel");
   userEvent.type(tel, "90807060");
 
@@ -60,13 +103,9 @@ beforeEach(async () => {
   userEvent.type(city, "Trondheim");
 
   birthYear = container.querySelector("#birthYear");
-  userEvent.type(birthYear, "2000");
+  userEvent.type(birthYear, "");
 
-  password = container.querySelector("#password");
-  userEvent.type(password, "password");
-
-  password2 = container.querySelector("#password2");
-  userEvent.type(password2, "password");
+  
 
   button = container.querySelector("button");
 });
@@ -75,18 +114,23 @@ afterEach(() => {
   document.body.removeChild(container);
   container = null;
 });
+  
+
 
 describe("Registration component", () => {
-  it("will say to wrong password", () => {
-    userEvent.clear(password2);
-    userEvent.type(password2, "wrongPassword");
-    act(() => {
-      userEvent.click(button);
-    });
-    const errorText = screen.getByText(passwordError);
-    expect(errorText).toBeInTheDocument();
-  });
 
+  it("will get userdata", () => {
+
+    const profiletxt = container.querySelector(
+      "#firstName");
+           
+    console.log(profiletxt.value)
+     expect(profiletxt).toBeInTheDocument();
+    
+    
+    
+  });
+  
   it("will say to to young", () => {
     userEvent.clear(birthYear);
     userEvent.type(birthYear, (new Date().getFullYear() - 12).toString());
@@ -107,14 +151,16 @@ describe("Registration component", () => {
     expect(errorText).toBeInTheDocument();
   });
 
-  // use async and await in act when expecting component
-  // to render with a promise inside
-  it("will say email does not exist", async () => {
-    PostData.mockImplementation(() => Promise.reject(new Error("Email")));
+  it("will say no Data", async () => {
+    PostPutData.mockImplementation(() => Promise.reject({birthyear: ""}));
     await act(async () => {
       userEvent.click(button);
     });
-    const errorText = screen.getByText(emailError);
-    expect(errorText).toBeInTheDocument();
+    
   });
+
+  // use async and await in act when expecting component
+  // to render with a promise inside
+  
+ 
 });
