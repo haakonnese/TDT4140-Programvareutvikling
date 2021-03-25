7eed646d88e992857bcd3527884bb47417bbfeee
Håkon Nese <haaknes@stud.ntnu.no>
2021-03-25 19:09:25 +0100

    Test editAd, editUser feilet pga provider-feil #37

--- frontend/src/user/UserInfo/EditAd.js
+++ frontend/src/user/UserInfo/EditAd.js
@@ -1,21 +1,9 @@
 import { React, useState, useEffect } from "react";
 import { GetData } from "../../service/FetchData";
 import PropTypes from "prop-types";
-// import {
-//   Avatar,
-//   Button,
-//   CssBaseline,
-//   Typography,
-//   Container,
-//   TextField,
-// } from "@material-ui/core";
-// import { Autocomplete } from "@material-ui/lab";
-// import PostAdd from "@material-ui/icons/PostAdd";
-// import useStyles from "../../standardComponents/styles";
-// import InputTextField from "../../standardComponents/InputTextField";
 import { useHistory } from "react-router-dom";
 import RegisterAd from "../../ProductRegistration/RegisterAd";
-// import { phoneError } from "./errorMessages";
+import { connect } from "react-redux";
 
 // const categories = [
 //   { type: "Kjøretøy" },
@@ -31,7 +19,7 @@ EditAd.propTypes = {
   errorType: PropTypes.string,
 };
 
-export default function EditAd({ match }) {
+function EditAd({ match }) {
   // const classes = useStyles();
   const history = useHistory();
   const [details, setDetails] = useState(false);
@@ -52,61 +40,6 @@ export default function EditAd({ match }) {
         history.push("/404");
       });
   }, []);
-
-  // const [preview, setPreview] = useState(false);
-  // function previewImage(e) {
-  //   if (e.target.files[0]) {
-  //     const reader = new FileReader();
-  //     setDetails({ ...details, img: e.target.files[0] });
-  //     reader.onload = function () {
-  //       setPreview(reader.result);
-  //     };
-  //     reader.readAsDataURL(e.target.files[0]);
-  //   } else {
-  //     setPreview(false);
-  //     setDetails({ ...details, img: "" });
-  //   }
-  // }
-
-  // handle change in form
-  // const handleChange = (e) => {
-  //   const value = e.target.val;
-  //   const name = e.target.value;
-
-  //   setDetails({ ...details, {name}: value });
-  //   console.log(e.target.value);
-  // };
-  // handle submit from button
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   // error-handeling
-  //   let allow = true;
-  //   // send registration to database and then do something with the result
-  //   for (const key in details) {
-  //     if (details[key] === "" || details[key] == null) {
-  //       allow = false;
-  //     }
-  //   }
-  //   if (allow) {
-  //     const formData = new FormData();
-  //     formData.set("img", details.img);
-  //     formData.set("category", details.category);
-  //     formData.set("description", details.description);
-  //     formData.set("name", details.name);
-  //     formData.set("price", details.price);
-  //     formData.set("city", details.city);
-  //     formData.set("created_by_user", "");
-  //     history.push("/");
-  //     UpdateData("listing/register", formData, "multipart/form-data")
-  //       .then((result) => {
-  //         if (result) {
-  //           history.push("/");
-  //         }
-  //       })
-  //       .catch((e) => console.log(e));
-  //   }
-  // };
-
   return (
     <div>
       {details ? (
@@ -117,3 +50,8 @@ export default function EditAd({ match }) {
     </div>
   );
 }
+const mapStateToProps = (state) => {
+  return { loggedIn: state.loggedIn };
+};
+
+export default connect(mapStateToProps)(EditAd);
