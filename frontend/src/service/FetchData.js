cda8e3bf51e271575ebed82fe96bf617bb3ec513
Håkon Nese <haaknes@stud.ntnu.no>
2021-03-25 10:36:01 +0100

    Fjerne duplikat #11

--- frontend/src/service/FetchData.js
+++ frontend/src/service/FetchData.js
@@ -1,44 +1,5 @@
 import { BASE_URL } from "./sellPointURL";
 
-export function PostPutData(
-  type,
-  userData,
-  contentType = "application/json",
-  method = "POST"
-) {
-  // fetches data from the given endpoint and returns the promise (responseJson)
-  return new Promise((resolve, reject) => {
-    const headers = {
-      Accept: "application/json",
-      // "Content-Type": contentType,
-    };
-    if (localStorage.getItem("token") != null) {
-      headers.Authorization = "Token " + localStorage.getItem("token");
-    }
-    let body;
-    if (contentType === "multipart/form-data") {
-      body = userData;
-    } else {
-      headers["Content-Type"] = contentType;
-      body = JSON.stringify(userData);
-    }
-    fetch(BASE_URL + type, {
-      method: method,
-      headers: headers,
-      body: body,
-    })
-      .then((response) => {
-        if (response.ok || response.created) {
-          return response.json();
-        } else {
-          throw new Error(response.status);
-        }
-      })
-      .then((responseJson) => resolve(responseJson))
-      .catch((error) => reject(error));
-  });
-}
-
 export function GetData(type, userData = null) {
   // fetches data from the given endpoint and returns the promise (responseJson)
   let text;
